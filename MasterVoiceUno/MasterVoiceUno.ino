1bdbac78eff9fd117e1602409434030d7fbfabe8
HeYanwei <heyw@yeefung.com>
2019-12-08 16:42:40 +0800

    配置VSCODE的开发

--- MasterVoiceUno/MasterVoiceUno.ino
+++ MasterVoiceUno/MasterVoiceUno.ino
@@ -6,11 +6,9 @@
  * 3. 如果连接正常，把灯点亮，如果连接异常，把灯熄灭
  */
 
-#include <ESP8266WiFi.h>
-
-const int echoPin = 12;
-const int trigPin = 13;
-const int ledPin = 14;
+const int echoPin = 6;
+const int trigPin = 7;
+const int ledPin = 5;
 
 struct SerialMsg{
   unsigned short header;
@@ -21,9 +19,6 @@ struct SerialMsg{
   unsigned short tail; 
 };
 
-WiFiServer server(80);
-WiFiClient client;
-bool alreadyConnected = false;
 bool isLightOn = false;
 int onCount = 0;
 
@@ -31,26 +26,11 @@ int onCount = 0;
 void setup() {
 
   Serial.begin(115200, SERIAL_8E2);//波特率115200，数据位8，偶校验，停止位2
-
+  //Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(echoPin, INPUT);
   digitalWrite(ledPin, LOW);
-
-  //Serial.print("开始连接...");
-  //Serial.println("YFRobot");
-  WiFi.mode(WIFI_STA);
-  WiFi.begin("YFRobot", "yfjqr2015");
-
-  while(WiFi.status()!=WL_CONNECTED) {
-    CalcDistance();
-  }
-  //Serial.println();
-  //Serial.println("WIFI已经连接");
-
-  server.begin();  
-  //Serial.print("服务端已启动，IP地址为");
-  //Serial.println(WiFi.localIP());
 }
 
 void SendDistanceMsg(long distance){
@@ -58,7 +38,7 @@ void SendDistanceMsg(long distance){
   unsigned int dist1=distance;
   struct SerialMsg msg;
   msg.header=0x6666;
-  msg.len=sizeof(SerialMsg)-2;
+  msg.len=sizeof(SerialMsg);
   msg.distance1 = dist1;
   msg.distance2 = 0;
   msg.distance3 = 0;
@@ -66,7 +46,8 @@ void SendDistanceMsg(long distance){
   
   unsigned char sendCs[50];
   memcpy(sendCs, &msg, sizeof(SerialMsg));
-  Serial.write(sendCs, sizeof(SerialMsg)-2);
+  Serial.write(sendCs, sizeof(SerialMsg));
+  //Serial.println(distance);
 }
 
 //测距//
@@ -89,42 +70,11 @@ void CalcDistance(){
   }
 
   SendDistanceMsg(cm);
-  if(alreadyConnected){
-    client.println(cm);
-  }
 
   delay(300);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  while(!alreadyConnected){
-    CalcDistance();
-    //Serial.println("Wait Client Connect...");
-    client = server.available();
-    if(client){
-      alreadyConnected=true;
-      client.flush();
-      client.println("OK***");
-
-      //Serial.print("客户端连接...");
-      //Serial.println(client.remoteIP());
-    }
-  }
-  if(!client) {   
-    alreadyConnected=false;
-    CalcDistance();
-    return;
-  }
-
-  String data = "";
-  while(client.available()>0){
-    data+=char(client.read());
-  }
-  if(data.length()>0){
-    //Serial.println(data);
-    client.println(data);
-  }
-  
   CalcDistance();
 }
